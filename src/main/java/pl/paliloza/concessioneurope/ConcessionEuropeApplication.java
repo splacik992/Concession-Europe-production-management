7359c1973da2821adb06c4004c722f90736892e4
Rafał Paliwoda <r.paliwoda992@gmail.com>
2021-03-15 19:14:43 +0100

    Initial Project.

---
+++ src/main/java/pl/paliloza/concessioneurope/ConcessionEuropeApplication.java
@@ -0,0 +1,13 @@
+package pl.paliloza.concessioneurope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcessionEuropeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcessionEuropeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/paliloza/concessioneurope/ConcessionEuropeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.paliloza.concessioneurope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcessionEuropeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
