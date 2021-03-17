72eb82f348aabda6d91610fb639edd65efe6bc8b
Daniel <danielloza1108@gmail.com>
2021-03-17 20:27:28 +0100

    Design start page

--- src/main/java/pl/paliloza/concessioneurope/controllers/MainController.java
+++ src/main/java/pl/paliloza/concessioneurope/controllers/MainController.java
@@ -5,12 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/")
 public class MainController {
 
-    @GetMapping
+    @GetMapping("/")
     public String viewMainModel(){
 
         return "index";
     }
+
 }
+
