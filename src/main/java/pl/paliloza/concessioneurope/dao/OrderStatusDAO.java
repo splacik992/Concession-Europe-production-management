c8a49658bbcd80b989522f11fb48408b742daa93
Daniel <danielloza1108@gmail.com>
2021-03-17 21:41:47 +0100

    Change thymeleaf to jsp

--- src/main/java/pl/paliloza/concessioneurope/dao/OrderStatusDAO.java
+++ src/main/java/pl/paliloza/concessioneurope/dao/OrderStatusDAO.java
@@ -1,8 +1,14 @@
 package pl.paliloza.concessioneurope.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import pl.paliloza.concessioneurope.entity.OrderStatus;
 
-public interface OrderStatusDAO extends JpaRepository<OrderStatus,Long> {
+import java.util.List;
 
+@Repository
+public interface OrderStatusDAO extends JpaRepository<OrderStatus,Long> {
+    @Query(value ="select name from order_status" ,nativeQuery = true)
+    List<OrderStatus> findAll();
 }
